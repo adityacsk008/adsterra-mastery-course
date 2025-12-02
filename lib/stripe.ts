7e992d4360bd8a4de6e00776a0576375fe16b85e
adityacsk008 <digitaladitya402@gmail.com>
2025-12-02 19:09:27 +0530

    Simplify Stripe - make it optional

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,5 @@
-import Stripe from 'stripe'
-import { loadStripe } from '@stripe/stripe-js'
+// Stripe client - Optional for now
+// You can add credentials later when ready
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
-})
-
-export const getStripe = () => {
-  return loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
-}
+export const stripe = null
+export const getStripe = () => null
